20abda3ffc6251b3a40ad81d85fab35ae6cb3138
brianmourik <brianmourik@live.nl>
2021-09-21 10:20:59 +0200

    tafelmanieren

---
+++ tafelmanieren/tafels-van.py
@@ -0,0 +1,5 @@
+
+a = int(input("Tafel van: "))
+
+for i in range(1,11):
+    print(a * i)
---
+++ tafelmanieren/tijd.py
@@ -0,0 +1,5 @@
+for a in range(0, 12):
+    print(str(a) + str("AM"))
+
+for p in range(12, 24):
+    print(str(p) + str("PM"))
